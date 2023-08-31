aeea45f16a6172d57c450e0f653d53bc2832fb81
Priyanshu Sidar <97578019+Wannabe-King@users.noreply.github.com>
2023-09-01 02:24:37 +0530

    quik app done

---
+++ android/app/src/main/kotlin/com/example/quikpick/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quikpick
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'QuikPick',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> {
+
+  int? currentChoice;
+
+  int count=0;
+
+  final myController=TextEditingController();
+
+  @override
+  void dispose(){
+    myController.dispose();
+    super.dispose();
+}
+
+  List<String> choices = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if(currentChoice==null)
+            const Text('What are the options??'),
+          if (currentChoice != null)
+            Text(
+              choices[currentChoice!],
+              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            )
+          else
+             Padding(
+              padding: const EdgeInsets.symmetric(horizontal:70.0),
+              child: TextField(
+                controller: myController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Option...',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+              ),
+            ),
+          if(currentChoice==null)
+            Text('Option count: $count'),
+          const Padding(padding: EdgeInsets.only(bottom: 40.0)),
+          if (currentChoice == null)
+            TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  addChoice(myController.text);
+                },
+                child: const Text('Add Option')),
+                const Padding(padding: EdgeInsets.only(bottom: 20.0)),
+          TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+            onPressed: () {
+              updateChoice();
+            },
+            child: const Text('Pick One'),
+          ),
+            const Padding(padding: EdgeInsets.only(bottom: 20.0)),
+          if (currentChoice != null)
+            TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+            onPressed: () {
+              reset();
+            },
+            child: const Text('Reset'),
+            )
+        ],
+      )),
+    );
+  }
+
+  void updateChoice() {
+    final random = Random();
+    int index = random.nextInt(choices.length);
+    setState(() {
+      currentChoice = index;
+    });
+  }
+
+  void addChoice(String choice) {
+    setState(() {
+      choices.add(choice);
+      count++;
+    });
+  }
+
+  void reset(){
+    setState(() {
+      currentChoice=null;
+      count=0;
+      choices.clear();
+    });
+  }
+}
